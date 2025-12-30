70b8c25c4b224d9ed93b6b69d84679163975cea7
senthurveld <150390304+senthurveld@users.noreply.github.com.>
2025-12-30 10:46:05 +0530

    Initial commit

---
+++ src/modules/Admin.js
@@ -0,0 +1,7 @@
+import { User } from "./User.js";
+
+export class Admin extends User{
+    constructor(name, email) {
+        super(name, email)
+    }
+}
---
+++ src/modules/Book.js
@@ -0,0 +1,10 @@
+export class Book {
+    constructor(title, author, genre) {
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.isAvailable = true;
+    }
+}
+
